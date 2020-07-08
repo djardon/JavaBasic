dddb65fc0d7931077eb9815769f1a626ef32274b
David Jardon <d.jardon@gmail.com>
2020-07-08 08:39:54 +0200

    Add Student new functions

--- src/com/ds/Student.java
+++ src/com/ds/Student.java
@@ -2,6 +2,8 @@ package com.ds;
 
 public class Student {
     //********* Vars *********/
+    private static final String school = "Neoland";
+
     // Default all vars accessibility are 'Public'
     String name;
     String email;
@@ -10,9 +12,15 @@ public class Student {
     double height;
 
     //********* Constructor *********/
+    public Student() { }
+
     public Student(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public static void printSchool() {
+        System.out.println("School " + school);
     }
 
     //********* Public functions *********/
@@ -20,6 +28,37 @@ public class Student {
         // '(weight / heightSquare())' operation return a double value
         // with '(float)' cast double to float
         return (float) (weight / heightSquare());
+    }
+
+    public String imcDescription() {
+        // menor 16.0 -> "Infrapeso: Delgadez Severa"
+        // mayor igual 16.0 y menor de 17 -> "Infrapeso: Delgadez moderada"
+        // mayor igual 17 y menor 19 -> "Infrapeso: Delgadez aceptable"
+        // mayor igual 19 y menor 25 -> "Peso Normal"
+        // mayor igual 25 y menor 30 -> "Sobrepeso"
+        // mayor igual 30 y menor igual 40 -> "Obeso: Tipo I"
+        // mayor 40 -> "Obeso: Tipo II"
+
+        float studentIMC = imc();
+        String description = "";
+
+        if(studentIMC < 16) {
+            description = "Infrapeso: Delgadez Severa";
+        } else if(studentIMC < 17) {
+            description = "Infrapeso: Delgadez moderada";
+        } else if(studentIMC < 19) {
+            description = "Infrapeso: Delgadez aceptable";
+        } else if(studentIMC < 25) {
+            description = "Peso Normal";
+        } else if(studentIMC < 30) {
+            description = "Sobrepeso";
+        } else if(studentIMC < 40) {
+            description = "Obeso: Tipo I";
+        } else {
+            description = "Obeso: Tipo II";
+        }
+
+        return description;
     }
 
     public boolean isOver18() {
