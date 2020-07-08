d25a2f586c61276cd0a0faf2930326f65cb98faa
David Jardon <d.jardon@gmail.com>
2020-07-08 08:39:35 +0200

    Add Subject class

---
+++ src/com/ds/Subject.java
@@ -0,0 +1,13 @@
+package com.ds;
+
+public class Subject {
+    String name;
+    int year;
+    String studentName;
+
+    //********* Constructor *********/
+    public Subject(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+}
