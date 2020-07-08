0a0a55134c60b6fcc90fee10a8f7fe50ba6982f5
David Jardon <d.jardon@gmail.com>
2020-07-08 18:06:47 +0200

    Add loop examples

---
+++ src/com/ds/loops/ForLoop.java
@@ -0,0 +1,43 @@
+package com.ds.loops;
+
+import com.ds.Subject;
+
+public class ForLoop {
+
+    public void doLoop() {
+        System.out.println("LOOPS FOR");
+        System.out.println("Print all ages between 0 and 18");
+        for (int age = 0; age < 18; age++) {
+            //System.out.println(age);
+        }
+
+        System.out.println("Print all pair numbers between 2 and 100");
+        for (int pair = 2; pair < 100; pair = pair + 2) {
+            //System.out.println(pair);
+        }
+
+        System.out.println("Print all pair numbers between 2 and 20");
+        for (int pair = 2; pair < 100; pair = pair + 2) {
+            if(pair == 20) {
+                System.out.println("Stop 'for' at " + pair);
+                // Exit loop
+                break;
+            }
+            //System.out.println(pair);
+        }
+
+        System.out.println("Print all pair numbers between 2 and 100 exclude 20");
+        for (int pair = 2; pair < 100; pair = pair + 2) {
+            if(pair == 20) {
+                // Jump to next iteration
+                continue;
+            }
+            // System.out.println(pair);
+        }
+
+        System.out.println("Print all SubjectTypes");
+        for (Subject.SubjectType type : Subject.SubjectType.values()) {
+            //System.out.println(type);
+        }
+    }
+}
---
+++ src/com/ds/loops/WhileLoop.java
@@ -0,0 +1,32 @@
+package com.ds.loops;
+
+public class WhileLoop {
+
+    public void doLoop() {
+        System.out.println("LOOPS WHILE");
+
+        System.out.println("Print all ages between 0 and 18");
+        // Print all ages between 0 and 18
+        int age = 0;
+        while (age < 18) {
+            //System.out.println(age);
+            age++;
+        }
+
+        System.out.println("Print all pair numbers between 2 and 100");
+        // Print all pair numbers between 2 and 100
+        int pair = 2;
+        while (pair < 100) {
+            //System.out.println(pair);
+            pair = pair + 2;
+        }
+
+        System.out.println("Print all numbers between 0 and 77");
+        // Print all numbers between 0 and 77
+        int number = 0;
+        do {
+            //System.out.println(number);
+            number++;
+        } while (number < 77);
+    }
+}
