739e6d788f6e7bec43689ec87b2eb2a9f8552eae
David Jardon <d.jardon@gmail.com>
2020-07-08 10:48:55 +0200

    Add imc type enum

--- src/com/ds/Student.java
+++ src/com/ds/Student.java
@@ -11,6 +11,17 @@ public class Student {
     float weight;
     double height;
 
+    enum ImcType {
+        DELGADEZ_SEVERA,
+        DELGADEZ_MODERADA,
+        DELGADEZ,
+        NORMAL,
+        SOBREPESO,
+        SOBREPESO_MODERADO,
+        SOBREPESO_SEVERO
+    }
+
+
     //********* Constructor *********/
     public Student() { }
 
@@ -31,34 +42,31 @@ public class Student {
     }
 
     public String imcDescription() {
-        // menor 16.0 -> "Infrapeso: Delgadez Severa"
-        // mayor igual 16.0 y menor de 17 -> "Infrapeso: Delgadez moderada"
-        // mayor igual 17 y menor 19 -> "Infrapeso: Delgadez aceptable"
-        // mayor igual 19 y menor 25 -> "Peso Normal"
-        // mayor igual 25 y menor 30 -> "Sobrepeso"
-        // mayor igual 30 y menor igual 40 -> "Obeso: Tipo I"
-        // mayor 40 -> "Obeso: Tipo II"
-
         float studentIMC = imc();
-        String description = "";
+        return calculateImcDescription(studentIMC);
+    }
 
-        if(studentIMC < 16) {
-            description = "Infrapeso: Delgadez Severa";
-        } else if(studentIMC < 17) {
-            description = "Infrapeso: Delgadez moderada";
-        } else if(studentIMC < 19) {
-            description = "Infrapeso: Delgadez aceptable";
-        } else if(studentIMC < 25) {
-            description = "Peso Normal";
-        } else if(studentIMC < 30) {
-            description = "Sobrepeso";
-        } else if(studentIMC < 40) {
-            description = "Obeso: Tipo I";
+    public ImcType imcType() {
+        float imc = imc();
+        ImcType typeIMC;
+
+        if(imc < 16) {
+            typeIMC = ImcType.DELGADEZ_SEVERA;
+        } else if(imc < 17) {
+            typeIMC = ImcType.DELGADEZ_MODERADA;
+        } else if(imc < 19) {
+            typeIMC = ImcType.DELGADEZ;
+        } else if(imc < 25) {
+            typeIMC = ImcType.NORMAL;
+        } else if(imc < 30) {
+            typeIMC = ImcType.SOBREPESO;
+        } else if(imc < 40) {
+            typeIMC = ImcType.SOBREPESO_MODERADO;
         } else {
-            description = "Obeso: Tipo II";
+            typeIMC = ImcType.SOBREPESO_SEVERO;
         }
 
-        return description;
+        return typeIMC;
     }
 
     public boolean isOver18() {
@@ -74,5 +82,34 @@ public class Student {
     //********* Private functions *********/
     private double heightSquare() {
         return height * height;
+    }
+
+    private String calculateImcDescription(float imc) {
+        // menor 16.0 -> "Infrapeso: Delgadez Severa"
+        // mayor igual 16.0 y menor de 17 -> "Infrapeso: Delgadez moderada"
+        // mayor igual 17 y menor 19 -> "Infrapeso: Delgadez aceptable"
+        // mayor igual 19 y menor 25 -> "Peso Normal"
+        // mayor igual 25 y menor 30 -> "Sobrepeso"
+        // mayor igual 30 y menor igual 40 -> "Obeso: Tipo I"
+        // mayor 40 -> "Obeso: Tipo II"
+        String description = "";
+
+        if(imc < 16) {
+            description = "Infrapeso: Delgadez Severa";
+        } else if(imc < 17) {
+            description = "Infrapeso: Delgadez moderada";
+        } else if(imc < 19) {
+            description = "Infrapeso: Delgadez aceptable";
+        } else if(imc < 25) {
+            description = "Peso Normal";
+        } else if(imc < 30) {
+            description = "Sobrepeso";
+        } else if(imc < 40) {
+            description = "Obeso: Tipo I";
+        } else {
+            description = "Obeso: Tipo II";
+        }
+
+        return description;
     }
 }
