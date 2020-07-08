c4d325471d9d183e43d781b163d329439a027388
David Jardon <d.jardon@gmail.com>
2020-07-08 10:48:38 +0200

    Add subject type enum

--- src/com/ds/Subject.java
+++ src/com/ds/Subject.java
@@ -2,12 +2,31 @@ package com.ds;
 
 public class Subject {
     String name;
+    SubjectType type;
     int year;
     String studentName;
 
+    public enum SubjectType {
+        iOS,
+        ANDROID,
+        FULLSTACK,
+        DATA_SCIENCE
+    }
+
     //********* Constructor *********/
-    public Subject(String name, int year) {
+    public Subject(String name, int year, SubjectType type) {
         this.name = name;
         this.year = year;
+        this.type = type;
+    }
+
+
+    //********* Public functions *********/
+    public void printSubjectType() {
+        switch (type) {
+            case iOS -> System.out.println("Subject type: " + type);
+            case ANDROID -> System.out.println("Subject type: " + type);
+            default -> System.out.println("Subject type: " + type);
+        }
     }
 }
